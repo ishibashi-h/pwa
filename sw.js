4350dab666fb653ad8cfb3c5db11422f5baaca32
ishibashi_h <ishibashi_h@jp.fujitsu.com>
2020-11-06 14:25:18 +0900

    fix

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
+// フェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function (event) { });
+
+// キャッシュファイルの設定と、インストール処理とキャッシュロード処理などを記述
+// note: https://qiita.com/tsunet111/items/450728056ba92d35508c
